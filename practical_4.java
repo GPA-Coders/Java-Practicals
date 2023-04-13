e2d654eb18ed930620796448adb6588de1238a8a
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-13 15:14:13 +0530

    Update practical_4.java

--- practical_4.java
+++ practical_4.java
@@ -1,14 +1,42 @@
-public class practical_4 {
-    public static void main(String[] args) {
-        int [][] mat1 = { {1, 2, 3} , {4, 5, 6} , {7, 8, 9} };
-        int [][] mat2 = { {1, 2, 3} , {4, 5, 6} , {7, 8, 9} };
-        int [][] result = new int [3][3];
-        for (int i=0;i<mat1.length;i++) {
-            for (int j=0;j<mat1[i].length;j++) {
-                result[i][j] = mat1[i][j] + mat2[i][j];
-                System.out.print(result[i][j] + " ");
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int [][] a = new int[3][3];
+        int [][] b = new int[3][3];
+        int [][] c = new int[3][3];
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print("Enter element at a[" + i + "][" + j + "] : ");
+                a[i][j] = sc.nextInt();
             }
-            System.out.print("\n");
+        }
+        System.out.println();
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print("Enter element at b[" + i + "][" + j + "] : ");
+                b[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                c[i][j] = a[i][j] + b[i][j];
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
+
+// Enter element at a[0][0] : 1
+//            ...
+// Enter element at a[2][2] : 9
+
+// Enter element at b[0][0] : 1
+//            ...
+// Enter element at b[2][2] : 9
+// 2 4 6 
+// 8 10 12
+// 14 16 18
