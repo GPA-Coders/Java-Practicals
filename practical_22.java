c145f5ccfa2f136b0da63b7c2adbdf81f741d6f3
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-23 12:10:27 +0530

    Update practical_22.java

--- practical_22.java
+++ practical_22.java
@@ -4,3 +4,12 @@ public class practical_22 {
         System.out.println("Hello");
     }
 }
+// Demo.java
+// package Second;
+// import First.*;
+// public class Demo {
+//     public static void main(String[] args) {
+//         practical_22 obj = new practical_22();
+//         obj.greet();
+//     }
+// }
