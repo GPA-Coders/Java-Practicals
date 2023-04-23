ab46075cb45d4ed7a6962204b373c9cf7125daf4
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-23 12:15:29 +0530

    Update and rename practical_10.java to 10.java

--- 10.java
+++ 10.java
@@ -2,7 +2,7 @@ public class practical_10 {
     public static void main(String[] args) {
         final int age = 18;
 
-        age = 45;        //create an error as it cannot assign vaue to final variable.
+        age = 45;        //will issue an error as it cannot assign vaue to final variable.
 
         System.out.println("Age is : " + age);
     }
