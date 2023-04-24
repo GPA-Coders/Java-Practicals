e0151b78bac8186faa1bacad77c30d522b024ac6
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-24 14:53:14 +0530

    Update practical_11.java

--- practical_11.java
+++ practical_11.java
@@ -1,16 +1,19 @@
 class Shape {
-    static float area (float radius) {
+    float area (float radius) {
         float pi = 3.1415f;
         return pi*radius*radius;
     }
-    static float area (float length,float width) {
+    float area (float length, float width) {
         return width * length;
     }
 }
-public class practical_11 {
+public class Main {
     public static void main(String[] args) {
         Shape s = new Shape();
-        System.out.println(s.area(5.5f));
-        System.out.println(s.area(5.5f , 7.3f));
+        System.out.println("Circle : " + s.area(5.5f));
+        System.out.println("Rectangle : " + s.area(5.5f , 7.3f));
     }
 }
+
+// Circle : 95.03037
+// Rectangle : 40.15
