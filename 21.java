fc2a05ab558e92265fc6dd67d5cdbcf254140234
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-24 15:26:24 +0530

    Update and rename practical_21.java to 21.java

--- 21.java
+++ 21.java
@@ -1,9 +1,9 @@
 final class Student {
     public void hello() {
-        System.out.println("Hello...");
+        System.out.println("Hello");
     }
 }
-public class practical_21 {
+public class Main {
     public static void main(String[] args) {
         Student s = new Student();
         s.hello();
