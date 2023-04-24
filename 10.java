34d702053f25a4f783ec1530d763e8e4b01f92f2
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-24 14:50:37 +0530

    Update 10.java

--- 10.java
+++ 10.java
@@ -1,9 +1,13 @@
-public class practical_10 {
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) {
-        final int age = 18;
-
-        age = 45;        //will issue an error as it cannot assign vaue to final variable.
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age : ");
+        final int age = sc.nextInt();
         System.out.println("Age is : " + age);
     }
 }
+
+// Enter age : 18
+// Age is : 18
