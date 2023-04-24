b7d885c9895975ac62370e38452ee30a14127f6a
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-24 15:10:48 +0530

    Update and rename practical_17.java to 17.java

--- 17.java
+++ 17.java
@@ -3,7 +3,7 @@ class Base {
         System.out.println("Base Class Constructor.");
     }
     public void greet() {
-        System.out.println("Hello EveryOne...");
+        System.out.println("Greetings from base.");
     }
 }
 class Derived extends Base {
@@ -11,12 +11,16 @@ class Derived extends Base {
         System.out.println("Derived Class Constructor.");
     }
     public void greet() {
-        System.out.println("Good Morning...");      //method overidden
+        System.out.println("Greetings from derived.");
     }
 }
-public class practical_17 {
+public class Main {
     public static void main(String[] args) {
         Derived d = new Derived();
         d.greet();
     }
 }
+
+// Base Class Constructor.
+// Derived Class Constructor.
+// Greetings from derived.
