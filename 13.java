91078d1f4114caab9cd54a4e68222c078eda0171
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-24 14:59:39 +0530

    Update and rename practical_13.java to 13.java

--- 13.java
+++ 13.java
@@ -1,15 +1,21 @@
-public class practical_13 {
+public class Main {
     public static void main(String[] args) {
-        String str = "He is Coder";
+        String str = "He is Hacker";
         System.out.println("charAt : " + str.charAt(3));
         System.out.println("Contains : " + str.contains("Code"));
-        System.out.println(str.format("Format : " + "%S",str));
+        System.out.println(str.format("Format : " + "%S", str));
         System.out.println("Length : " + str.length());
 
-        String st = "for@kudos";
+        String st = "for@hacker";
         String [] result = st.split("@");
         for (String a : result) {
-            System.out.println(a);
+            System.out.print(a + " ");
         }
     }
 }
+
+// charAt : i
+// Contains : false
+// Format : HE IS HACKER
+// Length : 12
+// for hacker
