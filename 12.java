693340496f07fcd9b451696fee428d5ad8e5c435
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-04-24 14:55:59 +0530

    Update and rename practical_12.java to 12.java

--- 12.java
+++ 12.java
@@ -7,15 +7,21 @@ class Employee {
         System.out.println("ID : " + id);
         System.out.println("Name : " + name);
     }
-    public Employee (int id,String name) {
+    public Employee (int id, String name) {
         this.id = id;
         this.name = name;
         System.out.println("ID : " + id);
         System.out.println("Name : " + name);
     }
 }
-public class practical_12 {
+public class Main {
     public static void main(String[] args) {
-        Employee e = new Employee(6,"Coder");
+        Employee e1 = new Employee();
+        Employee e2 = new Employee(6, "Coder");
     }
 }
+
+// ID : 123
+// Name : Hacker
+// ID : 6
+// Name : Coder
