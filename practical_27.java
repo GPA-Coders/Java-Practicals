89d586255f1a877d81e479240b0ada63ead714c9
GPA-Coders <118246765+GPA-Coders@users.noreply.github.com>
2023-06-08 19:34:12 +0530

    Add files via upload

---
+++ practical_27.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class practical_27 {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream fout = new FileOutputStream("MyFile.txt");
+            String s = "I Love Java!";
+            byte[] b = s.getBytes();
+            fout.write(b);
+            fout.close();
+            System.out.println("Writing Completed!");
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        }
+        try {
+            FileInputStream fin = new FileInputStream("MyFile.txt");
+            int data;
+            while((data = fin.read()) != -1) {
+                System.out.print((char) data);
+            }
+            System.out.println();
+            fin.close();
+            System.out.println("Reading Completed!");
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+// Writing Completed!
+// I Love Java!      
+// Reading Completed!
